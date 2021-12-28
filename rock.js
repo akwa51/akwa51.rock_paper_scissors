1591f0466f8a1cef3fc62f48936688093bdc5072
Akwasi Appiah <akappiah@gmail.com>
2021-12-28 17:14:54 +0000

    Index.html and rock.js modified

--- rock.js
+++ rock.js
@@ -1,15 +1,65 @@
 //An Array of Play Selection Outcome
 const playSelect=['paper','rock','scissors']
-// let userSelection;
+let userSelection;
 let computerSelection;
+let userWins=0;
+// const gameCounts=5;
+let gameOn=false;
 
-//Computer Random Selection Function
+//Computer Random Selection
 function computerPlay() {
     const arrayIndex=Math.floor(Math.random()*playSelect.length);
-    // console.log(playSelect[arrayIndex]);
     return (playSelect[arrayIndex]);
+}
+
+
+//User Play Selection
+function getUserPlay(){
+  //Ask User for Input in one of 3, trim and make LowerCase
+  userSelection= prompt('Select one of "Paper,Rock or Scissors" to Play').trim().toLowerCase();  
+
+  //Keep requesting While user selection is not found in defined Array 
+  while (playSelect.indexOf(userSelection)===-1) {
+   userSelection=prompt(`Your Choice: ${userSelection} is invalid, Please Play again`);
+    }
+
+   if (userSelection){gameOn=true;} 
+}
+
+
+// Play Game Once
+function playGameOnce(){
+    const drawGame=false;
+// Game accepts user and computer entries and shows outcome
+   if (userSelection===computerSelection){
+       alert(`Your choice of ${userSelection} is a draw. Play again!`)
+       drawGame=true;
+    //    break;
+    }else {
+        if (userSelection==='paper' && computerSelection==='rock') {
+            userWins+=1;
+            alert(`You Win ${userSelection} beats ${computerSelection}`);
+        }else if (userSelection==='rock' && computerSelection==='scissors') {
+            userWins+=1;
+            alert(`You Win ${userSelection} beats ${computerSelection}`);
+        }else if (userSelection==='scissors' && computerSelection==='paper'){
+            userWins+=1;
+            alert(`You Win ${userSelection} beats ${computerSelection}`);
+        }else{
+            alert (`You Lose ${computerSelection} beats ${userSelection}`);
+        }
+    }
+    gameOn=false;
 }
 
 computerSelection=computerPlay();
 console.log(computerSelection);
 
+userSelection=getUserPlay();
+console.log(userSelection)
+
+console.log (gameOn);
+
+if (gameOn) {
+    playGameOnce();
+}
