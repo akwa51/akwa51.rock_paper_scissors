d450ff3c42afc5b415680d679d5765c51650fbce
Akwasi Appiah <akappiah@gmail.com>
2021-12-29 00:42:24 +0000

    updated rock.js

--- rock.js
+++ rock.js
@@ -2,8 +2,8 @@
 const playSelect=['paper','rock','scissors']
 let userSelection;
 let computerSelection;
-let userWins=0;
-// const gameCounts=5;
+let userWins=userLoss=0;
+let gameCount=gameDraws=0;
 let gameOn=false;
 
 //Computer Random Selection
@@ -12,29 +12,38 @@ function computerPlay() {
     return (playSelect[arrayIndex]);
 }
 
-
 //User Play Selection
 function getUserPlay(){
+  let localUserInput;
   //Ask User for Input in one of 3, trim and make LowerCase
-  userSelection= prompt('Select one of "Paper,Rock or Scissors" to Play').trim().toLowerCase();  
+  localUserInput=prompt('Select one of "Paper,Rock or Scissors" to Play');
 
+  if (!localUserInput===null) {
+        userSelection= prompt('Select one of "Paper,Rock or Scissors" to Play').trim().toLowerCase();  
+  }
+  
   //Keep requesting While user selection is not found in defined Array 
   while (playSelect.indexOf(userSelection)===-1) {
-   userSelection=prompt(`Your Choice: ${userSelection} is invalid, Please Play again`);
-    }
+        localUserInput=prompt('Select one of "Paper,Rock or Scissors" to Play');
 
-   if (userSelection){gameOn=true;} 
+        if (!localUserInput===null) {
+            userSelection= prompt(`Your Choice: ${userSelection} is invalid, Please Play again`).trim().toLowerCase();  
+        }
+    }
+    localUserInput=null; 
+//    if (userSelection){gameOn=true;} 
+    if (userSelection){return true;} 
 }
 
 
 // Play Game Once
 function playGameOnce(){
-    const drawGame=false;
+    // let drawGame=false;
 // Game accepts user and computer entries and shows outcome
    if (userSelection===computerSelection){
        alert(`Your choice of ${userSelection} is a draw. Play again!`)
-       drawGame=true;
-    //    break;
+    //    drawGame=true;
+        gameDraws+=1;
     }else {
         if (userSelection==='paper' && computerSelection==='rock') {
             userWins+=1;
@@ -47,19 +56,42 @@ function playGameOnce(){
             alert(`You Win ${userSelection} beats ${computerSelection}`);
         }else{
             alert (`You Lose ${computerSelection} beats ${userSelection}`);
+            userLoss+=1;
         }
     }
     gameOn=false;
+    // reset all player choices 
+    userSelection=computerSelection=null;
+    gameCount+=1;
 }
 
-computerSelection=computerPlay();
-console.log(computerSelection);
 
-userSelection=getUserPlay();
-console.log(userSelection)
-
-console.log (gameOn);
-
-if (gameOn) {
-    playGameOnce();
+function displayPlayResults(){
+    if (userWins>0 && userLoss>0 && gameDraws>0) {
+        alert(`You won ${userWins} times, drew ${gameDraws} times and lost ${userLoss} times!!`);
+    }else if (userWins>0 && userLoss>0){
+        alert(`You won ${userWins} times and lost ${userLoss} times!`);
+    }else if (userWins>0 && gameDraws>0) {
+        alert(`You won ${userWins} times, drew ${gameDraws} times`);
+    }else if (userLoss>0 && gameDraws>0){
+        alert(`You Lost ${userLoss} times, drew ${gameDraws} times`,);
+    }else if (userLoss==0 && gameDraws==0) {
+        alert(`Congrats!You won ${userWins} times.`); 
+    }else if (userWins==0 && gameDraws==0){
+        alert(`Sorry!You lost ${userLoss} times.`); 
+    }
 }
+// console.log(computerSelection);
+// Play 5X Consecutively
+while (gameCount<=5){
+    //Computer Play
+   computerSelection=computerPlay();
+
+   // Get User choice
+    gameOn=getUserPlay(); 
+ 
+    if (gameOn) {
+        playGameOnce();
+    }  
+}
+if (gameCount>0) {displayPlayResults();}
